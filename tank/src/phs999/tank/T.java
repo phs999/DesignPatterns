12a7ae71e77b9ba4cd09f6107888b6854ca67f47
phs999 <peihesun@163.com>
2020-05-29 23:45:53 +0800

    新增一个继承自Frame的类，并重写paint方法，实现画出一个方框

--- tank/src/phs999/tank/T.java
+++ tank/src/phs999/tank/T.java
@@ -6,25 +6,8 @@ import java.awt.event.WindowEvent;
 
 public class T {
 	 public static void main(String[] args) {
-	        Frame f = new Frame();
-	        f.setSize(800,600);//像素px
-	        f.setResizable(false);//禁止改变大小
-	        f.setTitle("tank war");
-	        f.setVisible(true);//设置可见性
-	        //设置关闭
-	        f.addWindowListener(new WindowAdapter() {
-	            /**
-	             * Invoked when a window is in the process of being closed.
-	             * The close operation can be overridden at this point.
-	             *
-	             * @param e
-	             */
-	            @Override
-	            public void windowClosing(WindowEvent e) {
-	                //super.windowClosing(e);
-	                System.exit(0);
-	            }
-	        });
-	    }
+	       TankFrame tf = new TankFrame();
+	       
+	 }
 
 }
