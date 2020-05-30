18a15faece51779ac36087e8bdb8b2f241bfc356
phs999 <peihesun@163.com>
2020-05-30 22:39:58 +0800

    重写按键监听，支持同时按多个方向键控制方框移动

--- tank/src/phs999/tank/TankFrame.java
+++ tank/src/phs999/tank/TankFrame.java
@@ -7,54 +7,106 @@ import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class TankFrame extends Frame{
-	
-	int x=200,y=200;
+public class TankFrame extends Frame {
+
+	int x = 200, y = 200;
+
 	public TankFrame() {
-		setSize(800,600);//像素px
-		setResizable(false);//禁止改变大小
+		setSize(800, 600);// 像素px
+		setResizable(false);// 禁止改变大小
 		setTitle("tank war");
-		setVisible(true);//设置可见性
+		setVisible(true);// 设置可见性
 		addKeyListener(new MykeyListener());
-		//设置关闭
+		// 设置关闭
 		addWindowListener(new WindowAdapter() {
 			/**
-			 * Invoked when a window is in the process of being closed.
-			 * The close operation can be overridden at this point.
+			 * Invoked when a window is in the process of being closed. The close operation
+			 * can be overridden at this point.
 			 *
 			 * @param e
 			 */
 			@Override
 			public void windowClosing(WindowEvent e) {
-				//super.windowClosing(e);
+				// super.windowClosing(e);
 				System.exit(0);
 			}
 		});
 	}
 
-	
 	@Override
 	public void paint(Graphics g) {
-		//System.out.println("paint");
-		g.fillRect(x,y, 50, 50);
-		x+=10;
+		// System.out.println("paint");
+		g.fillRect(x, y, 50, 50);
+		//x += 10;
 	}
-	
-	class MykeyListener extends KeyAdapter{
 
+	class MykeyListener extends KeyAdapter {
+
+		boolean bL=false;
+		boolean bR=false;
+		boolean bU=false;
+		boolean bD=false;
+		
 		@Override
 		public void keyPressed(KeyEvent e) {
-			System.out.println("key pressed");
-			//x+=200;
-			//y+=10;
-			//repaint();
+			int key = e.getKeyCode();
+			switch (key) {
+			case KeyEvent.VK_LEFT:
+				bL=true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR=true;
+				break;
+			case KeyEvent.VK_UP:
+				bU=true;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD=true;
+				break;
+
+			default:
+				break;
+			}
+			// x+=200;
+			// y+=10;
+			// repaint();
+			if (bL) {
+				x-=10;
+			}
+			if (bR) {
+				x+=10;
+			}
+			if (bU) {
+				y-=10;
+			}
+			if (bD) {
+				y+=10;
+			}
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			System.out.println("key released");
+			int key = e.getKeyCode();
+			switch (key) {
+			case KeyEvent.VK_LEFT:
+				bL=false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR=false;
+				break;
+			case KeyEvent.VK_UP:
+				bU=false;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD=false;
+				break;
 
-		}
+			default:
+				break;
+			}
 		
+		}
 	}
+	
+	
 }
