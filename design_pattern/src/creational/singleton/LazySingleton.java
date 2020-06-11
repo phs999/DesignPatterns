915f369e95459f958ee7da0391086e92faa8a98f
phs999 <peihesun@163.com>
2020-06-12 00:06:26 +0800

    存在线程安全问题的懒汉式单例模式

---
+++ design_pattern/src/creational/singleton/LazySingleton.java
@@ -0,0 +1,15 @@
+package creational.singleton;
+
+public class LazySingleton {
+	private static LazySingleton lazySingleton;
+	private LazySingleton() {
+		
+	}
+	public static LazySingleton getInstance() {
+		if (lazySingleton==null) {
+			lazySingleton=new LazySingleton();
+		}
+		return lazySingleton;
+	}
+
+}
