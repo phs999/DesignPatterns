c6cc68d93db4c096b8362ad2e48f188760c3c167
phs999 <peihesun@163.com>
2020-08-23 12:11:59 +0800

    builder构建者模式-设计模式

---
+++ design_pattern/src/creational/builder/Location.java
@@ -0,0 +1,11 @@
+package creational.builder;
+
+public class Location {
+    String street;
+    String roomNo;
+
+    public Location(String street, String roomNo) {
+        this.street = street;
+        this.roomNo = roomNo;
+    }
+}
---
+++ design_pattern/src/creational/builder/Main.java
@@ -0,0 +1,13 @@
+package creational.builder;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person.PersonBuilder()
+                .basicInfo(1, "zhangsan", 18)
+                //.score(20)
+                .weight(200)
+                //.loc("bj", "23")
+                .build();
+        System.out.println(p.toString());
+    }
+}
