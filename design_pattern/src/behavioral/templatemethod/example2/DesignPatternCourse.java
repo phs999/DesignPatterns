3e1545af7f026f287c189add8f33ddcce88c6861
phs999 <peihesun@163.com>
2020-08-29 23:49:14 +0800

    template模板方法模式-设计模式

---
+++ design_pattern/src/behavioral/templatemethod/example2/DesignPatternCourse.java
@@ -0,0 +1,14 @@
+package behavioral.templatemethod.example2;
+
+public class DesignPatternCourse extends ACourse {
+    @Override
+    void packageCourse() {
+        System.out.println("提供课程Java源代码");
+    }
+
+    @Override
+    protected boolean needWriteArticle() {
+        return true;
+    }
+
+}
